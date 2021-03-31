2a0d9612ee6cee50ca0e16cb6bd0a0f5fb9da90d
dhanikprakash <dhanikprakash@gmail.com>
2021-03-31 20:57:48 +0100

    routing changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  HashRouter,
+} from "react-router-dom";
 import Header from "./components/header/Header";
 import HomePage from "./components/home/Homepage";
 import Checkout from "./components/checkout/Checkout";
+import Homepage from './components/home/Homepage';
 
 function App() {
   return (
     <>
-      <Router>
-        <div className="App">
-          <Switch>
-            <Route path="/Checkout" component={Checkout}>
-              <Header />
-              <Checkout />
-            </Route>
-            <Route path="/login">
-              <h1>Login page</h1>
-            </Route>
-            <Route path="/">
-              <Header />
-              <HomePage />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <HashRouter basename={process.env.PUBLIC_URL}>
+        <Header />
+        <Route
+          render={({ location }) => (
+            <Switch location={location}>
+              <Route path="/Checkout" component={Checkout}>
+              </Route>
+              <Route path="/login">
+                <h1>Login page</h1>
+              </Route>
+              <Route path="/" component={Homepage}></Route>
+            </Switch>
+          )}
+        />
+      </HashRouter>
     </>
   );
 }
